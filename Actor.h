a71d012e91fe40e522a2cb4ac439d1f3b1b986a6
Enuwbt <100519374+Enuwbt@users.noreply.github.com>
2022-07-10 23:32:48 +0900

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -3,8 +3,21 @@
 
 class Actor {
 public:
+  void* getUniqueID();
+  void* getRuntimeID();
   bool isBaby();
   void* getBlockPosCurrentlyStandingOn(Actor*,void*);
+};
+
+//@return UniqueID*
+void* Actor::getUniqueID(){
+  int v4 = *(uintptr_t*)(this + 732);
+  return (*(void (__fastcall **)(__int64 *))(*(_DWORD *)v4 + 1004))((__int64 *)this);
+}
+
+//@return RuntimeID*
+void* Actor::getRuntimeID(){
+  return reinterpret_cast<void*>(*(uintptr_t*)(this + 1008));
 }
 
 bool Actor::isBaby(){
